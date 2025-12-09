807a5f2942a25f889d6246679ec88c0b1bb76df6
Audry Wolters <audrywolters@gmail.com>
2025-12-09 16:27:03 -0600

    created method that collects the card companies that have invalid card numbers

--- credit-card-validator.js
+++ credit-card-validator.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("test");
 button.addEventListener("click", function() {
-	findInvalidCards(batch)
+	idInvalidCardCompanies(invalidCardNumbers)
 });
 
 
@@ -26,6 +26,9 @@ const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
 const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]; 
 const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]; 
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]; 
+
+// invalid credit card numbers
+const invalidCardNumbers = [invalid1, invalid2, invalid3, invalid4, invalid5];
 
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
@@ -65,10 +68,11 @@ const prepareNumber = (cardNumber) => {
 	}
 }
 
-// loop through the nested arrays
 
+
+// find invalid cards in the array of all card numbers
 const findInvalidCards = (batch) => {
-	let invalidCards = [];
+let invalidCards = [];
 
 	batch.forEach(cardNumbers => {
 		const isValid = validateCreditCard(cardNumbers);
@@ -80,3 +84,37 @@ const findInvalidCards = (batch) => {
 	return invalidCards;
 }
 
+const idInvalidCardCompanies = (invalidCardNumbers) => {
+	let companies = [];
+
+	invalidCardNumbers.forEach(invalidCardNumbers => {
+		let firstDigit = invalidCardNumbers[0];
+		
+		switch (firstDigit) {
+			case 3: 
+				if (companies.indexOf("Amex") === -1) {
+					companies.push("Amex");
+				}
+				break;
+			case 4:
+				if (companies.indexOf("Visa") === -1) {
+					companies.push("Visa");
+				}
+				break;
+			case 5:
+				if (companies.indexOf("Mastercard") === -1) {
+					companies.push("Mastercard");
+				}
+				break;
+			case 6:
+				if (companies.indexOf("Discover") === -1) {
+					companies.push("Discover");
+				}
+				break;
+			default:
+				console.log("Company not found");
+		}
+	});
+
+	return companies;
+}
